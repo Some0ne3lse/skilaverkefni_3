a1d8c67f5ae8932126fe1f114b1b67f316fb36cf
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2023-10-11 21:23:18 +0000

    Creating base files

---
+++ bin/skilaverkefni3.dart
@@ -0,0 +1,5 @@
+import 'package:skilaverkefni3/skilaverkefni3.dart' as skilaverkefni3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${skilaverkefni3.calculate()}!');
+}
---
+++ test/skilaverkefni3_test.dart
@@ -0,0 +1,8 @@
+import 'package:skilaverkefni3/skilaverkefni3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
